aa75ad7ca015b3c6f4d1376122a0fceb0d63045e
Paul Nelson <ultrono@gmail.com>
2023-08-21 09:34:17 +0200

    experiments

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (load (concat user-emacs-directory "init-bare.el"))
 
+;; (load (concat user-emacs-directory "init-test3.el"))
+(load (concat user-emacs-directory "init-elpaca.el"))
 ;; (load (concat user-emacs-directory "init-package.el"))
 ;; (load (concat user-emacs-directory "init-straight.el"))
-(load (concat user-emacs-directory "init-elpaca.el"))
-;; (load (concat user-emacs-directory "init-test2.el"))
 ;; (load (concat user-emacs-directory "init-test.el"))
