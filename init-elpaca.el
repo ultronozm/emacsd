e57dd75fa7165aa0928e8dfa8bdbeda5b963e62e
Paul Nelson <ultrono@gmail.com>
2023-08-21 09:34:24 +0200

    try org 9.7?

--- init-elpaca.el
+++ init-elpaca.el
@@ -54,6 +54,35 @@
 
 (elpaca-wait)
 
+(use-package org
+  :elpaca `(org
+	     :fork (:host nil
+		    :repo "https://git.tecosaur.net/tec/org-mode.git"
+		    :branch "dev"
+		    :remote "tecosaur")
+	     :files (:defaults "etc")
+	     :build t
+	     :pre-build
+	     (with-temp-file "org-version.el"
+	       (require 'lisp-mnt)
+	       (let ((version
+		     (with-temp-buffer
+		       (insert-file-contents "lisp/org.el")
+		       (lm-header "version")))
+		     (git-version
+		     (string-trim
+		      (with-temp-buffer
+			(call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+			(buffer-string)))))
+		 (insert
+		  (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+		  (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+		  "(provide 'org-version)\n")))
+	     :pin nil))
+
+(elpaca-wait)
+
+
 ;;; ------------------------------ GENERAL ------------------------------
 
 (setq custom-file (concat user-emacs-directory "init-custom.el"))
