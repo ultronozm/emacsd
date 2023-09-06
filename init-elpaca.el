4fcd10cf6466ea6d60f4e422d7361c391fe2cd5c
Paul Nelson <ultrono@gmail.com>
2023-09-06 10:58:56 +0200

    enable transient-mark-mode after some mark-* commands

--- init-elpaca.el
+++ init-elpaca.el
@@ -1347,3 +1347,38 @@ The list is ordered from bottom to top."
       (window-parameters (mode-line-format . none))))))
 
     
+(setq transient-mark-mode nil)
+(defun spw/remap-mark-command (command &optional map)
+  "Remap a mark-* command to temporarily activate Transient Mark mode."
+  (let* ((cmd (symbol-name command))
+         (fun (intern (concat "spw/" cmd)))
+         (doc (concat "Call `"
+                      cmd
+                      "' and temporarily activate Transient Mark mode.")))
+    (fset fun `(lambda ()
+                 ,doc
+                 (interactive)
+                 (call-interactively #',command)
+                 (activate-mark)))
+    (if map
+        (define-key map (vector 'remap command) fun)
+      (global-set-key (vector 'remap command) fun))))
+
+(dolist (command '(mark-word
+                   mark-sexp
+                   mark-paragraph
+                   mark-defun
+                   mark-page
+                   mark-whole-buffer
+                   rectangle-mark-mode))
+  (spw/remap-mark-command command))
+(with-eval-after-load 'org
+  (spw/remap-mark-command 'org-mark-element org-mode-map)
+  (spw/remap-mark-command 'org-mark-subtree org-mode-map))
+
+;; sometimes a key to just activate the mark is wanted
+(global-set-key "\M-i" (lambda () (interactive) (activate-mark)))
+;; resettle the previous occupant
+(global-set-key "\M-I" #'tab-to-tab-stop)
+
+
