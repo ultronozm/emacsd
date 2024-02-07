3813a6754430eae96aa40d4a9124dc49ccb4d8d7
Paul Nelson <ultrono@gmail.com>
2024-02-07 09:14:05 +0100

    test

--- init.el
+++ init.el
@@ -1,8 +1,10 @@
 (setq use-package-verbose t)
 (load (concat user-emacs-directory "init-bare.el"))
 
-;; (load (concat user-emacs-directory "init-test4.el"))
+;; (load (concat user-emacs-directory "init-test2.el"))
 (load (concat user-emacs-directory "init-elpaca.el"))
+
+;; (load (concat user-emacs-directory "init-test4.el"))
 ;; (load (concat user-emacs-directory "init-package.el"))
 ;; (load (concat user-emacs-directory "init-straight.el"))
 ;; (load (concat user-emacs-directory "init-test.el"))
