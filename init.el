cb6b71f0411f6e217ffb2484206728f74a36aad7
Paul Nelson <ultrono@gmail.com>
2024-03-20 18:19:09 +0100

    experimenting

--- init.el
+++ init.el
@@ -2,5 +2,6 @@
 (load (concat user-emacs-directory "init-bare.el"))
 (load (concat user-emacs-directory "init-settings.el"))
 ;; (load (concat user-emacs-directory "init-without-latex.el"))
+;; (load (concat user-emacs-directory "latex-minimal.el"))
 (load (concat user-emacs-directory "init-main.el"))
 ;; (load (concat user-emacs-directory "init-test3.el"))
