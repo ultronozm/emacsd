e2501eae18cbf081034841126631747fb50afaa2
Paul Nelson <ultrono@gmail.com>
2024-04-26 10:36:37 +0200

    add easy-kill

--- init-main.el
+++ init-main.el
@@ -1581,3 +1581,6 @@ The value of `calc-language` is restored after BODY has been processed."
                        (TeX-master-file)))))
 
 (add-hook 'clone-indirect-buffer-hook 'set-TeX-master-from-cloned)
+
+(use-package easy-kill)
+(global-set-key [remap kill-ring-save] #'easy-kill)
