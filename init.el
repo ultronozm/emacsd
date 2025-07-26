2fd848672b16f2e91f6eba2053dbc20d6ced96aa
Paul Nelson <ultrono@gmail.com>
2025-07-26 13:03:49 +0400

    upgrade to elpaca 0.11

--- init.el
+++ init.el
@@ -1098,7 +1098,7 @@ If the predicate is true, add NAME to `repo-scan-repos'."
 
 ;;; elpaca
 
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -1133,9 +1133,10 @@ If the predicate is true, add NAME to `repo-scan-repos'."
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
+
 (when (eq window-system 'w32)
   (elpaca-no-symlink-mode))
 
