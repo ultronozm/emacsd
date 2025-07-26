8b2b2562b64a9bc27b2c9de2ffa712092e6bf269
Paul Nelson <ultrono@gmail.com>
2025-07-26 12:53:03 +0200

    add use-package forms to imenu at top-level

--- early-init.el
+++ early-init.el
@@ -1,6 +1,9 @@
 ;; -*- lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
+(eval-after-load 'lisp-mode
+  `(add-to-list 'lisp-imenu-generic-expression
+                    (list nil ,use-package-form-regexp-eval 2)))
 (setq use-package-enable-imenu-support t)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
