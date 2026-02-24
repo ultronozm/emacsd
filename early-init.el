79e1a5488be38bbd7f123310ee718cd9f0802ad5
Paul Nelson <ultrono@gmail.com>
2026-02-24 15:23:43 +0100

    early-init: disable initial fullscreen frame

--- early-init.el
+++ early-init.el
@@ -17,7 +17,8 @@
 (setq use-package-enable-imenu-support t)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
-(setq initial-frame-alist '((fullscreen . fullboth)
+(setq initial-frame-alist '(
+                            ;; (fullscreen . fullboth)
                             (vertical-scroll-bars . nil)))
 (setq default-frame-alist '((vertical-scroll-bars . nil)
                             ;; (width . 80) ;; default
